3f20e1c9269e870007feebebfb94a1267a1915c9
xajohnso <57449824+xajohnso@users.noreply.github.com>
2021-07-16 23:09:52 -0500

    Update Main.java

--- Supermarket Logistics/src/main/Main.java
+++ Supermarket Logistics/src/main/Main.java
@@ -1,20 +1,61 @@
 package main;
 
+import java.util.Scanner;
+
 import person.Shopper;
+import product.Product;
 import product.Shelf;
 
 public class Main {
-
+		
+	static Shopper shopper = new Shopper();
+	static Shelf shelf = new Shelf();
+	static Product product = new Product();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Shopper Xavier = new Shopper();
-		Shelf shelf = new Shelf();
 		
+		Scanner scanner = new Scanner(System.in);
+		start(shopper, shelf);
+		shop(shopper, shelf, scanner);
+		scanner.close();
+	}
+	
+	public static void start(Shopper shopper, Shelf shelf) {
 		shelf.newShelf();
-		Xavier.addCash(500.00);
-		shelf.addToCart(Xavier);
+		shopper.addCash(500.00);
+	}
+	
+	public static void shop(Shopper shopper, Shelf shelf, Scanner scanner) {
+		int zz = 0;
+
+		do {
+		System.out.println("What product would you like?");
+		shelf.showShelf();
+		int productChoice = scanner.nextInt();
 		
+		System.out.println("How much of product would you like?");
+		int quantityChoice = scanner.nextInt();
+		
+		System.out.println("Is this right? 1) Yes 2) No \nProduct: " + shelf.getProduct(productChoice) + "\nQuantity: " + quantityChoice);
+		int finCheck = scanner.nextInt();
+		if (finCheck == 1) {
+			shelf.updateShelf(shelf.getProduct(productChoice), quantityChoice);
+			shopper.addToCart(shelf.getProduct(productChoice), quantityChoice);
+			System.out.println("Is this all you want?");
+			shopper.printCart();
+			System.out.println("1) Yes 2) No");
+			int fc = scanner.nextInt();
+			if (fc == 1) {
+				priceTotal(shopper, shelf);
+				zz++;
+			} else {}
+		} else {}
+		} while (zz == 0);
+	}
+	
+	public static void priceTotal(Shopper shopper, Shelf shelf) {
+		shopper.cartToSet();
+		shopper.getItems();
 	}
 
 }
