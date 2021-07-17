7a60a8ae677f51d4a9c689f565254bdc055325d4
xajohnso <57449824+xajohnso@users.noreply.github.com>
2021-07-16 23:12:36 -0500

    Update Product.java

--- Supermarket Logistics/src/product/Product.java
+++ Supermarket Logistics/src/product/Product.java
@@ -1,15 +1,30 @@
 package product;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Product {
 
-	protected LinkedHashMap<String, Integer> products = new LinkedHashMap<String, Integer>();
+	protected LinkedHashMap<String, Integer> productsPackages = new LinkedHashMap<String, Integer>();
+	protected HashMap<String, Double> productsPrice = new HashMap<String, Double>();
 	
 	public void setProducts() {
-		products.put("Chicken", 6);
-		products.put("Milk", 6);
-		products.put("Candy", 6);
-		products.put("Powerade", 6);
+		productsPackages.put("Chicken", 6);
+		productsPackages.put("Milk", 6);
+		productsPackages.put("Candy", 6);
+		productsPackages.put("Powerade", 6);
+	}
+	
+	public void setPrices() {
+		productsPrice.put("Chicken", 7.50);
+		productsPrice.put("Milk", 6.35);
+		productsPrice.put("Candy", 11.67);
+		productsPrice.put("Powerade", 20.11);
+	}
+	
+	public double getProductsPrice(String productName) {
+		double price = productsPrice.get(productName);
+		
+		return price;
 	}
 }
