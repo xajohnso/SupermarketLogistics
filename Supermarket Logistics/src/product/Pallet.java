e4e1b09ca82cf369757be7baa7df46e622922427
xajohnso <57449824+xajohnso@users.noreply.github.com>
2021-07-19 21:57:55 -0500

    Update Pallet.java

--- Supermarket Logistics/src/product/Pallet.java
+++ Supermarket Logistics/src/product/Pallet.java
@@ -1,5 +1,16 @@
 package product;
 
+import java.util.HashMap;
+
 public class Pallet {
 
+	protected HashMap<String, Integer> pallet = new HashMap<String, Integer>();
+	
+	public void removeItems(String product) {
+		pallet.remove(product);
+	}
+	
+	public void getItem(String product) {
+		pallet.get(product);
+	}
 }
